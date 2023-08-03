e1117f6513a6f726312e1e46e7afdd549cab409f
pollitux <raul@tingsystems.com>
2023-08-03 11:18:53 -0600

    add model comment

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Comment
 
 
 # Register your models here.
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'priority', 'crated_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
--- api/models.py
+++ api/models.py
@@ -4,13 +4,27 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Task(models.Model):
+class AbstractModel(models.Model):
+    crated_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+
+
+class Task(AbstractModel):
     title = models.CharField(max_length=180)
     description = models.TextField(null=True, blank=True)
     priority = models.IntegerField(
         choices=((0, 'low'), (1, 'medium'), (2, 'high'))
     )
-    crated_at = models.DateTimeField(default=timezone.now)
     expired_at = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return f"#{self.id} {self.title}"
 
+
+class Comment(AbstractModel):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name='comment_task'
+    )
+    comment = models.TextField()
